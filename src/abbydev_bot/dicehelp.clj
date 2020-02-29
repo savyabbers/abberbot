d217883f47851f6fc9c28cd320e8b9139b9c92e7
savyabbers <adamvince24@gmail.com>
2020-02-29 17:29:12 -0500

    added extra function for when user only wants to roll one die ex 'd20'. before the limit checker would fail due to parsing not giving die-count

--- src/abbydev_bot/dicehelp.clj
+++ src/abbydev_bot/dicehelp.clj
@@ -13,13 +13,18 @@
     true
     nil))
 
+(defn check-dice [dice]
+  (if (contains? dice :die-count)
+    dice
+    (conj dice {:die-count 1})))
+
 (defn roll [arg]
   (let [user-dice (try
               (dice/parse arg)
               (catch Exception e nil))]
     (if user-dice
       (do
-        (if (dice-limiter? user-dice)
+        (if (dice-limiter? (check-dice user-dice))
           (dice/die-rolls-to-str 10 (dice/roll user-dice))
           "Sorry, The limits for the dice are 100d100"))
       "Error parsing. use /rollhelp for info")))
